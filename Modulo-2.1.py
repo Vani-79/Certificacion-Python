4407c92d70dccf30a0716c6f09f041f800bd21b4
unknown <yalfaro145@gmail.com>
2024-03-26 16:19:52 -0300

    Inicio

---
+++ Modulo-2.1.py
@@ -0,0 +1,26 @@
+# Print
+
+# \n Hace un Salto de Linea, Parecido a print()
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+
+# Print posee dos argumentos de Palabra clave (end y sep) y estos constan del argumento (end) un = y el valor que tendra el argumento, estos siempre se deben usar despues del ultimo argumento de posicion
+
+print("Mi nombre es", "Python.", end="")
+print("Monty Python.")
+
+# Pyton de normal separa los datos en espacios cuando se utilizan comas en la cadena de texto de un print, pero esto se puede cambiar utilizando la palabra clave "sep"
+
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+
+# Tambien se pueden usar juntos
+
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+# Resultado esperado: Programming***Essentials***in...Python
+
+print("Programming","Essentials","in", sep="***",end="...")
+print("Python")
---
+++ Modulo-2.2.1.py
@@ -0,0 +1,27 @@
+# Literales
+
+# Exponentes, ejemplo 3 x 10 elevado a la 8 en python se usa e o E para ejecutar exponentes basa 10
+
+print(3e8)
+
+# Python siempre elige la presentación más corta del número, y esto se debe de tomar en consideración al crear literales. 
+
+print(0.0000000000000000000001) # Resultado esperado: 0.0000000000000000000001
+# Resultado Obtenido: "1e-22" ya que es la forma mas corta de expresar el numero.
+
+# Como escribir algo con comillas dosbles en una cadena de texto
+
+# Ejemplo 1
+print("Me gusta \"Monty Python\"")
+# Ejemplo 2
+print('Me gusta "Monty Python"')
+# Ejemplo 3
+print('"Estoy"\n""aprendiendo""\n"""Ptyhon"""')
+
+#  Como insertar un apóstrofe en una cadena la cual está limitada por dos apóstrofes
+
+# Ejemplo 1
+print('I\'m Monty Python.')
+# Ejemplo 2
+print("I'm Monty Python.")
+
