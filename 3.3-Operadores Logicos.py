1dcde6d2a327103f03868d97449e9c889ba932c2
CETECOM <yalfaro145@gmail.com>
2024-04-15 11:54:49 -0400

    inicio modulo 3.3

---
+++ 3.3-Operadores Logicos.py
@@ -0,0 +1,21 @@
+#Operadores
+
+# And operador binario con una prioridad inferior a la expresada por los operadores de comparación, sin el uso de parentesis
+#counter > 0 and value == 100
+
+# Or tiene prioridad mas baja que And (igual a + y -)
+
+# Not operador unario que realiza una negación lógica. Su funcionamiento es simple: convierte la verdad en falso y lo falso en verdad
+
+# Ejemplo 1:
+var = 1
+print(var > 0)
+print(not (var <= 0))
+
+
+# Ejemplo 2:
+print(var != 0)
+print(not (var == 0))
+
+
+
